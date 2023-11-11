94606936c53c043dfc4446a55d31d191a622fba7
EVGENII LUKIN <eugeniylukin@gmail.com>
2023-11-12 00:24:14 +0100

    fixed interaction with gpt

--- generate/DocGptGenerator.py
+++ generate/DocGptGenerator.py
@@ -1,15 +1,16 @@
 import json
 import os
-import openai
 
 from parse.constants import EXTRACT_FOLDER, PACKAGE_SOURCE_MAP_FILE
 from generate.prompts import *
+from openai import OpenAI
 
 
 class DocGptGenerator:
     def __init__(self, repository_names):
         self.__repo_names = repository_names
-        self.__openai_key = os.getenv("OPENAI_API_KEY")
+        self.__openai_key = os.environ.get("OPENAI_API_KEY")
+        self.__client = OpenAI()
 
     def generate_doc(self):
         # Read json with package names
@@ -21,32 +22,30 @@ class DocGptGenerator:
         with open(json_file_name, 'r') as json_file:
             package_map = json.load(json_file)
 
-        print("######################################### GENERATING ################################################")
+        print("Generating docs...")
         # Ask gpt the initial question
-        initial_question = INITIAL_PROMPT + SOURCES_EXTENSION_HINT + 'python'
-        print(initial_question)
-        print(f"{self.__get_response_from_gpt(initial_question)}")
+        self.__get_response_from_gpt(INITIAL_PROMPT)
         # Ask gpt about files
         for package, files in package_map.items():
             if package.split("-")[0] not in self.__repo_names:
                 print(f"Package: {package} not in repo_names, skipping")
                 continue
             for file in files:
-                self.__ask_gpt_about_file(file)
+                with open(file, 'r') as file_content:
+                    file_question = FILE_QUESTION + file_content.read()
+                    self.__get_response_from_gpt(file_question)
         # Ask gpt final question
-        print(f"{self.__get_response_from_gpt(GENERAL_QUESTION)}")
-
-    def __ask_gpt_about_file(self, file):
-        file_question = FILE_QUESTION + file
-        print(f"{self.__get_response_from_gpt(file_question)}")
+        self.__get_response_from_gpt(GENERAL_QUESTION)
 
     def __get_response_from_gpt(self, script):
-        # Send a request to the model
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=script,
-            max_tokens=100,
-            api_key=self.__openai_key
+        print(f"{script}")
+        completion = self.__client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "You are a system analyst who should analyze incoming files of a project and understand "
+                            "what this project does."},
+                {"role": "user", "content": script}
+            ]
         )
-        # Get the AI's response
-        return response.choices[0].text
+        print(f"{completion.choices[0].message}")
